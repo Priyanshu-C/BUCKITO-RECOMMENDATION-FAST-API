a1307f2732e54ef7d576f7faacefcf645ffb3628
cybertronayush <44187942+cybertronayush@users.noreply.github.com>
2021-01-24 15:39:38 +0530

    Add files via upload

--- content.py
+++ content.py
@@ -5,7 +5,7 @@ import requests
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from tmdbv3api import Movie, TMDb
-
+import imdb
 
 df = pd.read_csv('preprocessed_data.csv')
 df_cache = pd.read_csv('cache_data.csv')
@@ -15,6 +15,22 @@ movie_list = list(df['movie_title'])
 #later we'll use for other needs
 tmdb = TMDb()
 tmdb.api_key = '62fd9021dbeec142016bbfc8e3888baf'
+
+#Function for movie id of any Movie
+def movieiden(str):
+  ia = imdb.IMDb()
+  title = str
+  title.lower()
+# searching the name
+  search = ia.search_movie(title)
+
+# loop for printing the name and id
+  for i in range((1)):
+    # getting the id
+    id = search[i].movieID
+  # printing it
+    return (id)
+
 
 # This function take movie name from user, and return 10 similar type of movies.
 def recommendation(str):
@@ -43,8 +59,11 @@ def recommendation(str):
             # suggested movies are storing into a list
        suggested_movie_list = []
        for i in sim_score:
-                movie_index = i[0]
-                suggested_movie_list.append(df['movie_title'][movie_index])
+           movie_index = i[0]
+           suggested_movie_list.append(df['movie_title'][movie_index])
+       new_list_rec = []
+       for j in range(len(suggested_movie_list)):
+         new_list_rec.append(movieiden(suggested_movie_list[j]))
+         new_list_rec.append(suggested_movie_list[j])
 
-       return suggested_movie_list
-
+       return new_list_rec
